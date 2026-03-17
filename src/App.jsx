373b9baac6904bd46d02f9a76b8cb9709d3282e8
DiegoSanz27 <diegosanzm923@gmail.com>
2026-03-17 15:08:01 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App(){
+
+  return(
+    <div className="min-h-screen bg-green-800 flex items-center justify-center"> 
+      <h1 className="text-white text-4xl font-bold">
+        Con rico Programar tailwind v4 funciona full..
+      </h1>
+    </div>
+  )
+
+} 
